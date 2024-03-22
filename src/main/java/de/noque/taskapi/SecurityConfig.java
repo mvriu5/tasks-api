cbde895d4037e72ee0ad5bc61e89950ae9fbfca3
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-03-22 12:01:46 +0100

    commit

--- src/main/java/de/noque/taskapi/SecurityConfig.java
+++ src/main/java/de/noque/taskapi/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +21,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-                .oauth2LoginWithDefaultRedirection()
+                .oauth2Login()
                 .logout(logout -> logout
-                        .logoutSuccessURL(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + issuer)
+                        .logoutSuccessHandler(new SimpleUrlLogoutSuccessHandler() {
+                            {
+                                setDefaultTargetURL(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + issuer);
+                            }
+                        })
                 );
 
         return http.build();
--- src/main/java/de/noque/taskapi/controller/TaskController.java
+++ src/main/java/de/noque/taskapi/controller/TaskController.java
@@ -4,6 +4,7 @@ import de.noque.taskapi.model.Task;
 import de.noque.taskapi.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
